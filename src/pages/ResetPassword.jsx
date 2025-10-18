a2af1a982403d8c47b50f365a8b69164bdbbdb7e
Rhys Wickens <rhys.wickens@orennia.com>
2025-10-18 17:04:10 -0600

    try reset password again

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -2,18 +2,22 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 function ResetPassword() {
-  // useEffect(() => {
-  //   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+  useEffect(() => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
     
-  //   if (isMobile) {
-  //     const hash = window.location.hash
-  //     const search = window.location.search
+    if (isMobile) {
+      // Get the FULL hash fragment with tokens
+      const hash = window.location.hash  // #access_token=...&refresh_token=...&type=recovery
+      const search = window.location.search
       
-  //     const deepLink = `two-cents://resetPassword${search}${hash}`
+      // Pass tokens in the deep link
+      const deepLink = `two-cents://resetPassword${search}${hash}`
       
-  //     window.location.href = deepLink
-  //   }
-  // }, [])
+      window.location.href = deepLink
+    } else {
+      // Show message for desktop users
+    }
+  }, [])
 
   return (
     <div className="redirect-message">
@@ -24,18 +28,18 @@ function ResetPassword() {
         
         <div style={{ marginTop: '2rem' }}>
           <p>Please open the Two Cents app on your mobile device to continue.</p>
-          {/* <p style={{ fontSize: '0.9rem', marginTop: '1rem' }}>
+          <p style={{ fontSize: '0.9rem', marginTop: '1rem' }}>
             If you're on desktop, please click the password reset link from your email 
             while on your mobile device.
-          </p> */}
+          </p>
         </div>
 
-        <button 
+        {/* <button 
           className="redirect-button"
           onClick={() => window.location.href = 'two-cents://resetPassword'}
         >
           Open Two Cents App
-        </button>
+        </button> */}
 
         <div style={{ marginTop: '2rem' }}>
           <Link to="/" style={{ color: 'var(--grey)', fontSize: '0.9rem' }}>
